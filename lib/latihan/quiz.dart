5943f449ab276934235d03af4b4e273580f1cf46
ahmadsapey <sapeyamad.lp3i.krw24@gmail.com>
2025-09-25 21:45:34 +0700

    menambahkan tugas quiz

---
+++ lib/latihan/quiz.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class LatihanQuiz extends StatelessWidget {
+  const LatihanQuiz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan Quiz"),
+        backgroundColor: Colors.amber,
+        foregroundColor: Colors.blue,
+      ),
+      
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          
+          children: [
+            Text(
+              'Training',
+              style: TextStyle(fontSize: 18, 
+              fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 24),
+            Text(
+              'Your Program',
+              style: TextStyle(fontSize: 15, 
+              fontWeight: FontWeight.normal),
+            ),
+            SizedBox(height: 50),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.purpleAccent,
+                borderRadius: BorderRadius.circular(6),
+              ),
+              
+              child: Text(
+                'Lets Toning\nand Glutes Workout',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 25),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.cyan,
+                borderRadius: BorderRadius.circular(6),
+              ),
+              
+              child: Center(
+                child: Text(
+                  'You are doing great\nkeep it up\nstick to your plan',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
